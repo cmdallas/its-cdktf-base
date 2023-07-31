3cc08019ae56bf888cd76e10f8db5d5f1372ca0f
mason <mason@localhost.localdomain>
2023-07-30 23:24:10 -0600

    updated service principal name in boostrap script

--- bin/bootstrap_customer_account.sh
+++ bin/bootstrap_customer_account.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
-CUSTOMER=itscdev
-REGION=westus2
-SUBSCRIPTION=<subscriptionid>
+customer="itscdev"
+region="westus2"
+subscription_id="<subscriptionid>"
 
-RESOURCE_GROUP_NAME=tfstate-$CUSTOMER
-STORAGE_ACCOUNT_NAME=tfstate$CUSTOMER
-CONTAINER_NAME=tfstate-$CUSTOMER
+resource_group_name="tfstate-$customer"
+storage_account_name="tfstate$customer"
+container_name="tfstate-$customer"
+service_principal_name="its-cdktf-sp"
 
 
 # Create resource group
-az group create --name $RESOURCE_GROUP_NAME --location $REGION
+az group create --name "$resource_group_name" --location "$region"
 
 # Create storage account
-az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_ACCOUNT_NAME --sku Standard_LRS --location $REGION --encryption-services blob
+az storage account create --resource-group "$resource_group_name" --name "$storage_account_name" --sku Standard_LRS --location "$region" --encryption-services blob
 
 # Create blob container
-az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --public-access off
+az storage container create --name "$container_name" --account-name "$storage_account_name" --public-access off
 
-# Create cdktf service principal
-az ad sp create-for-rbac --name its-cdktf-service-principal --role Contributor --scopes /subscriptions/$SUBSCRIPTION
+# Create cdktf service principal and capture the output in a variable
+sp_output=$(az ad sp create-for-rbac --name "$service_principal_name" --role Contributor --scopes "/subscriptions/$subscription_id")
+
+# Extract the secret key from the output
+secret_key=$(echo "$sp_output" | jq -r .password)
 
 # Create key vault for service principal secret
-az keyvault create --name "its-cdktf-service-principal" --resource-group $RESOURCE_GROUP_NAME --location $REGION
+az keyvault create --name "$service_principal_name" --resource-group "$resource_group_name" --location "$region"
+
+# Set the service principal secret in the key vault
+az keyvault secret set --vault-name "$service_principal_name" --name "$service_principal_name" --value "$secret_key"
