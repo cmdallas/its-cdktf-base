c0a131bc7cf92e27cd9268c65fead3724046ca61
mason <mason@localhost.localdomain>
2023-07-30 20:54:27 -0600

    consolidated bootstrap scripts into bootstrap_customer_account.sh

--- bin/bootstrap_customer_account.sh
+++ bin/bootstrap_customer_account.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-CUSTOMER=<customer_acronym>
+CUSTOMER=itscdev
 REGION=westus2
+SUBSCRIPTION=<subscriptionid>
 
 RESOURCE_GROUP_NAME=tfstate-$CUSTOMER
 STORAGE_ACCOUNT_NAME=tfstate$CUSTOMER
@@ -16,3 +17,9 @@ az storage account create --resource-group $RESOURCE_GROUP_NAME --name $STORAGE_
 
 # Create blob container
 az storage container create --name $CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --public-access off
+
+# Create cdktf service principal
+az ad sp create-for-rbac --name its-cdktf-service-principal --role Contributor --scopes /subscriptions/$SUBSCRIPTION
+
+# Create key vault for service principal secret
+az keyvault create --name "its-cdktf-service-principal" --resource-group $RESOURCE_GROUP_NAME --location $REGION
